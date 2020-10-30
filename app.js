908fad6dfa1f7d4cc94d85af24a834ce19a851ed
albus-dumbeldore <dhruv0426@gmail.com>
2020-10-30 21:07:44 +0530

    Roiim Assignment-2020

---
+++ app.js
@@ -0,0 +1,169 @@
+
+const express       = require('express')
+const hbs           = require('hbs')
+const app           = express()
+var request         = require('request');
+const bodyParser    = require("body-parser")
+const mongoose      = require('mongoose')
+var http            = require('http')
+var server          = http.createServer(app)
+var port            = process.env.PORT || 3000
+
+mongoose.connect('mongodb://localhost/roiim',{useNewUrlParser:true,useUnifiedTopology:true})
+
+app.set('view engine','hbs')
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// ============================models============================================================
+var consumerIdSchema = new mongoose.Schema({
+    email:String,
+    id:String
+})
+
+var  ConsumerId= mongoose.model('ConsumerId',consumerIdSchema)
+// =================================================================================================
+
+app.get('/roiim',(req,res)=>{
+    res.render("Payment.ejs")
+})
+
+
+app.post('/roiim/customerid',(req,res)=>{
+    // res.send('Success')
+    // console.log(req)
+    var data=JSON.stringify(req.body)
+    data=JSON.parse(data)
+    var email=data.email
+    // console.log(typeof email)
+
+    ConsumerId.findOne({email:email}).then(result=>{
+        if(result){
+            // console.log('bc bhag na')
+            res.send(JSON.stringify(result))
+        }
+        else{
+            request({
+                url: 'https://api.test.paysafe.com/paymenthub/v1/customers', 
+                method :"POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4',
+                    'Simulator': 'EXTERNAL'
+                },
+                body: {
+                    "merchantCustomerId": email,
+                    "locale": "en_US",
+                    "firstName": data.firstName,
+                    "lastName": data.lastName,
+                    "dateOfBirth": {
+                        "year": 1990,
+                        "month": 7,
+                        "day": 1
+                    },
+                    "email": email,
+                    "phone": data.phone,
+                    "ip": "192.0.126.111",
+                    "gender": "M",
+                    "nationality": "Canadian",
+                    "cellPhone": "777-555-8888"
+                },
+                json:true
+                }, function (error, response, body) {
+                console.log('Status:', response.statusCode);
+                // console.log(error,body)
+                console.log(response.body.id)
+                // console.log(body.error)
+                // if(body.error){
+                //     res.end("error"); 
+                // }
+                
+                var newConsumerId = new ConsumerId({
+                    email:email,
+                    id:response.body.id
+                })
+                newConsumerId.save((err,user)=>{
+                    if(err)console.log(err)
+                    else{
+                        res.send(JSON.stringify(user))
+                    } 
+                })
+
+                // res.end(response.body.id)
+            }); 
+
+        }
+    }).catch(err=>{
+        res.send("error")
+    })
+})
+
+app.post('/roiim/customerToken',(req,res)=>{
+    var data=JSON.stringify(req.body)
+    data=JSON.parse(data)
+    console.log(data)
+
+    request({
+        url: 'https://api.test.paysafe.com/paymenthub/v1/customers/'+data.customerid+'/singleusecustomertokens', 
+        method :"POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4',
+            'Simulator': 'EXTERNAL'
+        },
+        body: {
+            "merchantRefNum": data.merchantRefNumber,
+            "paymentTypes": ["CARD"]
+        },
+        json:true
+        }, function (error, response, body) {
+        console.log('Status:', response.statusCode);
+        if(body.error){
+            res.end("error"); 
+        }
+        // console.log('ara')
+        res.end(JSON.stringify(response))
+    }); 
+
+})
+
+app.post('/roiim/payment',(req,res)=>{
+    var data=JSON.stringify(req.body)
+    data=JSON.parse(data)
+    // console.log('bcccccccccccccc')
+    // console.log(data)
+    request({
+        url: 'https://api.test.paysafe.com/paymenthub/v1/payments', 
+        method :"POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4',
+            'Simulator': 'EXTERNAL'
+        },
+        body: {
+            "merchantRefNum": data.merchantNumber,
+            "amount": parseInt(data.amount)*100,
+            "currencyCode": "USD",
+            "dupCheck": true,
+            "settleWithAuth": false,
+            "paymentHandleToken": data.token,
+            "customerIp": "10.10.12.64",
+            "description": "Magazine subscription",
+        },
+        json:true
+        }, function (error, response, body) {
+        console.log('Status:', response.statusCode);
+        // console.log(error,body)
+        // console.log(response)
+        // console.log(body.error)
+        if(body.error){
+            res.end("error"); 
+        }
+        res.end(JSON.stringify(response))
+    }); 
+})
+
+server.listen(port,()=>{
+    console.log('Server is on port ' + 3000)
+})
